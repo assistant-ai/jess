5d26b9f0f64520f9a13dd068bda20b6079604e49
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2023-04-14 18:41:43 -0700

    improved CLI version

--- main.go
+++ main.go
@@ -37,84 +37,102 @@ func main() {
 		{
 			Name:  "dialog",
 			Usage: "Manage dialogs",
-			Subcommands: []*cli.Command{
-				{
-					Name:  "list",
-					Usage: "List all conversations",
-					Action: func(c *cli.Context) error {
-						dialogIds, err := db.GetDialogIDs()
+			Action: func(c *cli.Context) error {
+				if c.Bool("list") {
+					dialogIds, err := db.GetDialogIDs()
+					if err != nil {
+						cli.Exit(err, 1)
+					} else {
+						jess_cli.PrintDialogIDs(dialogIds)
+					}
+					return nil
+				} else if c.String("continue") != "" {
+					id := c.String("continue")
+					if id == "" {
+						cli.Exit(errors.New("please provide dialog id"), 1)
+					} else {
+						// Replace with your actual logic to start a new conversation
+						fmt.Println("Starting a new conversation...")
+						ctx, err := initContext(apiKeyFilePath)
+						if err != nil {
+							cli.Exit(err, 1)
+						}
+						err = chat.StartChat(id, ctx)
+						if err != nil {
+							cli.Exit(err, 1)
+						}
+					}
+					return nil
+				} else if c.String("show") != "" {
+					id := c.String("show")
+					if id == "" {
+						cli.Exit(errors.New("please provide dialog id"), 1)
+					} else {
+						messages, err := db.GetMessagesByDialogID(id)
 						if err != nil {
 							cli.Exit(err, 1)
 						} else {
-							jess_cli.PrintDialogIDs(dialogIds)
+							chat.ShowMessages(messages)
 						}
-						return nil
-					},
+					}
+					return nil
+				} else if c.String("delete") != "" {
+					id := c.String("delete")
+					if id == "" {
+						cli.Exit(errors.New("please provide dialog id"), 1)
+					} else {
+						err := db.RemoveMessagesByDialogId(id)
+						if err != nil {
+							cli.Exit(err, 1)
+						}
+					}
+					return nil
+				}
+				return errors.New("no required key provided")
+			},
+			Flags: []cli.Flag{
+				&cli.BoolFlag{
+					Name:    "list",
+					Aliases: []string{"l"},
+					Usage:   "list all dialogs",
 				},
-				{
-					Name:  "continue",
-					Usage: "Continue a dialog with the specified ID",
-					Action: func(c *cli.Context) error {
-						id := c.Args().First()
-						if id == "" {
-							cli.Exit(errors.New("please provide dialog id"), 1)
-						} else {
-							// Replace with your actual logic to start a new conversation
-							fmt.Println("Starting a new conversation...")
-							ctx, err := initContext(apiKeyFilePath)
-							if err != nil {
-								cli.Exit(err, 1)
-							}
-							err = chat.StartChat(id, ctx)
-							if err != nil {
-								cli.Exit(err, 1)
-							}
-						}
-						return nil
-					},
+				&cli.StringFlag{
+					Name:    "continue",
+					Aliases: []string{"c"},
+					Usage:   "continue dialog with the id",
 				},
-				{
-					Name:  "show",
-					Usage: "Show conversation by ID",
-					Action: func(c *cli.Context) error {
-						id := c.Args().First()
-						if id == "" {
-							cli.Exit(errors.New("please provide dialog id"), 1)
-						} else {
-							messages, err := db.GetMessagesByDialogID(id)
-							if err != nil {
-								cli.Exit(err, 1)
-							} else {
-								chat.ShowMessages(messages)
-							}
-						}
-						return nil
-					},
+				&cli.StringFlag{
+					Name:    "show",
+					Aliases: []string{"s"},
+					Usage:   "show dialog with the id",
 				},
-				{
-					Name:  "delete",
-					Usage: "Delete conversation by ID",
-					Action: func(c *cli.Context) error {
-						id := c.Args().First()
-						if id == "" {
-							cli.Exit(errors.New("please provide dialog id"), 1)
-						} else {
-							err := db.RemoveMessagesByDialogId(id)
-							if err != nil {
-								cli.Exit(err, 1)
-							}
-						}
-						return nil
-					},
+				&cli.StringFlag{
+					Name:    "delete",
+					Aliases: []string{"d"},
+					Usage:   "delete dialog with the id",
 				},
 			},
 		},
 		{
 			Name:  "file",
 			Usage: "Process file input",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "path",
+					Aliases:  []string{"p"},
+					Usage:    "path to the file",
+					Required: true,
+				},
+				&cli.StringFlag{
+					Name:     "input",
+					Aliases:  []string{"i"},
+					Usage:    "prompt input to pass with the file",
+					Required: true,
+				},
+			},
 			Action: func(c *cli.Context) error {
-				filePath := c.Args().Get(0)
-				prompt := c.Args().Get(1)
+				filePath := c.String("path")
+				prompt := c.String("input")
 				ctx, _ := initContext(apiKeyFilePath)
 				b, _ := os.ReadFile(filePath)
 				input := model.FileInput{
