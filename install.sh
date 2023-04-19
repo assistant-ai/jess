cecd77818cae58ff8899dcffd5e3ba285013369e
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2023-04-18 19:32:36 -0700

    final release refactoring

--- install.sh
+++ install.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 set -e
 
-# Set the binary name, version, and repository URL here
+# Install jq if it is not installed already
+if ! command -v jq &> /dev/null; then
+  echo "jq not found, installing..."
+  case "$(uname)" in
+    "Linux")
+      sudo apt-get update
+      sudo apt-get install -y jq
+      ;;
+    "Darwin")
+      brew install jq
+      ;;
+    *)
+      echo "Unsupported OS. Please manually install jq"
+      exit 1
+      ;;
+  esac
+  echo "jq installed successfully!"
+fi
+
+# Set the binary name and repository URL here
 BINARY_NAME="jess"
-VERSION="v2"
-REPOSITORY="https://github.com/assistant-ai/jess/releases/download/${VERSION}"
+REPOSITORY="https://github.com/assistant-ai/jess/releases"
+
+# Obtain the latest pushed tag
+LATEST_TAG=$(curl --silent "https://api.github.com/repos/assistant-ai/jess/releases/latest" | jq -r .tag_name)
 
 # Check the OS and architecture to download the correct binary
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
@@ -12,7 +33,7 @@ ARCH="$(uname -m)"
 [ "$ARCH" == "x86_64" ] && ARCH="amd64"
 
 # Download the binary
-curl -L -o /tmp/${BINARY_NAME} "${REPOSITORY}/${BINARY_NAME}-${OS}-${ARCH}"
+curl -L -o /tmp/${BINARY_NAME} "${REPOSITORY}/download/${LATEST_TAG}/${BINARY_NAME}-${OS}-${ARCH}"
 
 # Make it executable and move it to /usr/local/bin
 chmod +x /tmp/${BINARY_NAME}
--- release.sh
+++ release.sh
@@ -3,3 +3,15 @@
 VERSION=$(cat VERSION)
 git tag -a "v$VERSION" -m "Release version $VERSION"
 git push --tags
+
+# Upload binary jess to the GitHub release
+gh release create "v$VERSION" --title "Release version $VERSION" --notes "Added logic to upload binary jess."
+
+upload_binary() {
+  local path=$1
+  gh release upload "v$VERSION" "${path}" --label "binary-jess"
+}
+upload_binary "./jess-darwin-arm64"
+upload_binary "./jess-darwin-amd64"
+upload_binary "./jess-linux-amd64"
+upload_binary "./jess-windows-amd64.exe"
