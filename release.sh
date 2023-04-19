6efca568d0909818876e89ae7eaa87e59a43592a
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2023-04-18 19:36:57 -0700

    version set to 4

--- release.sh
+++ release.sh
@@ -4,12 +4,15 @@ VERSION=$(cat VERSION)
 git tag -a "v$VERSION" -m "Release version $VERSION"
 git push --tags
 
+. ./build.sh
+
 # Upload binary jess to the GitHub release
 gh release create "v$VERSION" --title "Release version $VERSION" --notes "Added logic to upload binary jess."
 
-upload_binary() {
+upload_remove_binary() {
   local path=$1
-  gh release upload "v$VERSION" "${path}" --label "binary-jess"
+  gh release upload "v$VERSION" "${path}"
+  rm -rf "${path}"
 }
 upload_binary "./jess-darwin-arm64"
 upload_binary "./jess-darwin-amd64"
