96d586195a963a16206662b6462cf26be9687264
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2023-04-30 15:48:45 -0700

    fix release logic

--- release.sh
+++ release.sh
@@ -9,12 +9,12 @@ git push --tags
 # Upload binary jess to the GitHub release
 gh release create "v$VERSION" --title "Release version $VERSION" --notes "Added logic to upload binary jess."
 
-upload_remove_binary() {
+upload_and_remove_binary() {
   local path=$1
   gh release upload "v$VERSION" "${path}"
   rm -rf "${path}"
 }
-upload_binary "./jess-darwin-arm64"
-upload_binary "./jess-darwin-amd64"
-upload_binary "./jess-linux-amd64"
-upload_binary "./jess-windows-amd64.exe"
+upload_and_remove_binary "./jess-darwin-arm64"
+upload_and_remove_binary "./jess-darwin-amd64"
+upload_and_remove_binary "./jess-linux-amd64"
+upload_and_remove_binary "./jess-windows-amd64.exe"
