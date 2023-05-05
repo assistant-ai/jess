6d21a085e2b4c1e390319cbf37d60333587c3323
Slava Kovalevskyi <slava.kovalevskyi@getcruise.com>
2023-05-05 08:23:10 -0700

    Web UI is now reasonbly looking

--- rest/server.go
+++ rest/server.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"embed"
+	"io"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/handlers"
 	"github.com/assistant-ai/llmchat-client/db"
 	"github.com/assistant-ai/llmchat-client/gpt"
 )
+
+//go:embed client.html
+var clientHtml embed.FS
 
 func StartRest(gpt *gpt.GptClient) {
 	r := mux.NewRouter()
@@ -24,9 +29,32 @@ func StartRest(gpt *gpt.GptClient) {
         handlers.AllowedMethods([]string{"POST", "GET", "OPTIONS", "PUT", "DELETE"}),
         handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
     )
+	// Serve embedded client.html file on root route
+	r.HandleFunc("/", serveClientHtml)
 
 	fmt.Println("Starting server at :8080")
 	log.Fatal(http.ListenAndServe(":8080", cors(r)))
+}
+
+func serveClientHtml(w http.ResponseWriter, r *http.Request) {
+	// Get the contents of the client.html file
+	file, err := clientHtml.Open("client.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	// Read the contents of the file
+	content, err := io.ReadAll(file)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Set "Content-Type" header and write the contents of the file
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(content)
 }
 
 func getContextList(w http.ResponseWriter, r *http.Request) {
@@ -55,15 +83,23 @@ func sendMessageToChatGPT(w http.ResponseWriter, r *http.Request, gpt *gpt.GptCl
 	vars := mux.Vars(r)
 	contextId := vars["contextId"]
 
-	message := r.FormValue("message")
+	// Updated: Parse JSON from the request body
+	var requestBody map[string]string
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	message := requestBody["message"]
 	if message == "" {
 		http.Error(w, "message is required", http.StatusBadRequest)
 		return
 	}
-	
+
 	response, err := gpt.SendMessage(message, contextId)
 	if err != nil {
-		return 
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
