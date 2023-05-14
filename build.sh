f53b7f3a2b9f15655af7505ac61798cf7343da7a
Slava Kovalevskyi <slava.kovalevskyi@getcruise.com>
2023-05-13 19:07:31 -0700

    please work

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 VERSION=$(cat VERSION)
 
+export GOPATH="/go"
+
 gox -osarch="linux/amd64 darwin/amd64 darwin/arm64 windows/amd64" -ldflags="-X main.version=$VERSION" -output "jess-{{.OS}}-{{.Arch}}"
