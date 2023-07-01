dfb391a6a8202f017c7ea8418c902318db45badc
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2023-07-01 12:34:55 -0700

    Formatting upddate

--- utils/config_utils.go
+++ utils/config_utils.go
@@ -2,17 +2,18 @@ package utils
 
 import (
 	"os"
+	"path/filepath"
+
 	"github.com/b0noi/go-utils/v2/fs"
 	"github.com/spf13/viper"
-	"path/filepath"
 )
 
 type AppConfig struct {
-    OpenAiApiKeyPath       		string
-    ServiceAccountKeyPath  		string
-    ModelName              		string
-    GCPProjectId             	string
-	LogLevel					string
+	OpenAiApiKeyPath      string
+	ServiceAccountKeyPath string
+	ModelName             string
+	GCPProjectId          string
+	LogLevel              string
 }
 
 func LoadConfig(configPath string) (*AppConfig, error) {
@@ -27,11 +28,11 @@ func LoadConfig(configPath string) (*AppConfig, error) {
 
 	openAiApiKeyFilePath := filepath.Join(folderPath, "open-ai.key")
 	jessServiceAccountFilePath := filepath.Join(folderPath, "jess-service-account.json")
-    viper.SetDefault("openai.openai_api_key_path", openAiApiKeyFilePath)
-    viper.SetDefault("gcp.service_account_key_path", jessServiceAccountFilePath)
-    viper.SetDefault("model", "GPT3")
-    viper.SetDefault("gcp.gcp_project_id", "")
-    viper.SetDefault("log_level", "INFO")
+	viper.SetDefault("openai.openai_api_key_path", openAiApiKeyFilePath)
+	viper.SetDefault("gcp.service_account_key_path", jessServiceAccountFilePath)
+	viper.SetDefault("model", "GPT3")
+	viper.SetDefault("gcp.gcp_project_id", "")
+	viper.SetDefault("log_level", "INFO")
 
 	exists, err := fs.PathExists(configPath)
 
@@ -47,19 +48,19 @@ func LoadConfig(configPath string) (*AppConfig, error) {
 		defer file.Close()
 	}
 
-    viper.SetConfigFile(configPath)
-    err = viper.ReadInConfig()
-    if err != nil {
-        return nil, err
-    }
+	viper.SetConfigFile(configPath)
+	err = viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
 
-    appConfig := &AppConfig{
-        OpenAiApiKeyPath: viper.GetString("openai.openai_api_key_path"),
-        ServiceAccountKeyPath: viper.GetString("gcp.service_account_key_path"),
-        ModelName: viper.GetString("model"),
-        GCPProjectId: viper.GetString("gcp.gcp_project_id"),
-        LogLevel: viper.GetString("log_level"),
-    }
+	appConfig := &AppConfig{
+		OpenAiApiKeyPath:      viper.GetString("openai.openai_api_key_path"),
+		ServiceAccountKeyPath: viper.GetString("gcp.service_account_key_path"),
+		ModelName:             viper.GetString("model"),
+		GCPProjectId:          viper.GetString("gcp.gcp_project_id"),
+		LogLevel:              viper.GetString("log_level"),
+	}
 
-    return appConfig, nil
+	return appConfig, nil
 }
