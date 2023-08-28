157f4db9479cfa588023220ce6535e1664220b84
Nikita <5195769+nikkrichko@users.noreply.github.com>
2023-08-28 09:06:46 -0700

    MAIN: go:embed - does not work return to prev system

--- prompt_storage/text/user_story.go
+++ prompt_storage/text/user_story.go
@@ -1,6 +1,55 @@
 package text
 
-// go:embed user_story.md
-var staticPrompt string
+const UserStoryPrompt = "You need to use provided markdown instruction for generating user story:" + staticPrompt
 
-var UserStoryPrompt = "You need to use provided markdown instruction for generating user story:" + staticPrompt
+const staticPrompt = `
+# create users story according next steps
+## Subtasks
+1. **Persona and Role**
+    - Create a description of the persona for this user story and its background.
+    - Provide at least 5 characteristics of this persona.
+    - From the perspective of this person, generate a user story and describe the importance of this user story for this persona.
+
+2. **Industry Best Practices**
+    - Describe this user story summary from the perspective of industry best practices.
+    - Provide at least 10 items best practices that should be followed for this user story.
+
+3. **INVEST**
+    - In this section, rephrase and adopt the user story to the INVEST principle.
+    - Provide at least 10 items that should be followed for this user story.
+
+4. **Functionality or Improvement**
+    - Clearly explain the functionality or improvement the feature should bring.
+    - Use typical tasks for the provided topic.
+    - Provide at least 15 function requirements that should come. If it is possible to split each functionality into some sub-tasks, it should be under each functional requirement as an additional numerated list for each functional requirement.
+
+5. **Non-functional Requirements**
+    - Clearly explain the non-functional requirements that usually go for such kind of user stories.
+    - After non-functional requirements, provide a numerated list of sub-tasks that should cover each of that non-functional requirement that you've just provided.
+
+6. **Acceptance Criteria**
+    - Specify conditions that define when the feature is complete and functioning correctly.
+    - Use the best industries practice to generate acceptance criteria.
+    - After acceptance criteria, provide a numerated list of sub-tasks that should cover each of that acceptance criteria that you've just provided.
+
+7. **Business Value**
+    - Describe the benefits and value the feature will bring to users and the product.
+
+8. **Positive Test Cases**
+    - Give examples of expected behavior and successful outcomes for thorough testing.
+    - These test cases should contain at least 1 positive test case for each functional and non-functional requirement.
+
+9. **Negative Test Cases**
+    - Specify scenarios where the feature should handle errors, edge cases, or unexpected inputs gracefully.
+
+10. **Overview of Documentations**
+    - According to the main topic and all subtasks, provide an overview of documentations that should be created for this feature.
+
+11. **Typical Bugs and Pitfalls**
+    - Describe typical bugs and pitfalls that developers should avoid.
+    - It should contain at least the top 10 typical bugs (pitfalls) in this area.
+    - Give recommendations on how to avoid them.
+
+## Output Requirement
+The response would be in markdown, and each section should have its title and its number.
+YOu need return only answer without input tasks.`
